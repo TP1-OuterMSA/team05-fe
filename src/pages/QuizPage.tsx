e955b58b23380abb01cad841f8ec95a1c2983af5
soyeon <soyeon20031010@gmail.com>
2025-06-02 18:17:42 +0900

    [fix] api 연결 코드 수정 (#14)
    * init: 프로젝트 기본 세팅
    
    * feat:먹고싶은메뉴 UI
    
    * feat:제출모달창 구현
    
    * feat: 먹고싶은메뉴 페이지 api 연동
    
    * feat: 퀴즈 기능 구현
    
    * feat: foodList detail 페이지 구현
    
    * feat:랭킹 페이지, 평점퀴즈 페이지 추가
    
    * feat: 퀴즈 포인트 작업 중인 코드 저장
    
    * feat:내 순위 삭제
    
    * feat:평점 평균 0.1단위로 상승으로 수정
    
    * fix: 리뷰페이지 질문 api 연동
    
    * fix: 스타일 수정
    
    * feat:랭킹페이지 api연결
    
    * fix: 스타일 수정
    
    * fix: 사용하지 않는 모달 삭제제
    
    * feat: 관리자 페이지 메뉴 추가
    
    * feat:관리자페이지 api연결
    
    * feat: 퀴즈 내용 입력 페이지 구현
    
    * feat:메뉴 입력 갯수 수정
    
    * fix: api endpoint 변경
    
    * feat:맛집입력 관리자페이지UI, HEADER수정
    
    * feat: foodlist, foodlist admin 페이지 api 연결
    
    * fix: admin quiz 페이지 ui 수정
    
    * fix: 맛집 추가 카드 width 수정 및 페이지 리로드
    
    * test
    
    * feat: 평균 예측 퀴즈 UI구현
    
    * feat: 퀴즈평점결과페이지, qr 리뷰 페이지 api 연결결
    
    * feat: 사이드바 평점 평균 정답자 페이지 추가
    
    * fix: qr component 오류 수정
    
    * fix: qr 인식 수정
    
    * fix: 안쓰는 코드 삭제
    
    * feat:리뷰 모아보기 페이지 구현
    
    * feat: 리뷰 모아보기 페이지 api 연동
    
    * fix: 이미지 오류 수정
    
    ---------
    
    Co-authored-by: hayoung Park <mcd158@naver.com>

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -4,51 +4,49 @@ import * as S from "../styles/quiz/QuizPageStyle"
 import HalfCircleProgress from "../components/HalfCircleProgress";
 import FileImg from "../assets/images/team5/file.png";
 import Carrot from "../assets/images/team5/carrot.png";
-import { foodListDummy } from "../api/foodListDummy";
 import PhoneModal from "../components/modal/PhoneModal";
 import { getQuiz } from "../api/adminQuiz";
 
-interface CardProps{
-	id: number;
-	question: string;
-	correctChoiceId: number;
-	choiceDtoList: ChoiceDtoList[];
+interface CardProps {
+  id: number;
+  question: string;
+  correctChoiceId: number;
+  choiceDtoList: ChoiceDtoList[];
 }
 
-interface ChoiceDtoList{
-	id: number;
-	content: string;
+interface ChoiceDtoList {
+  id: number;
+  content: string;
 }
 
 export default function QuizPage() {
   const [currentQuiz, setCurrentQuiz] = useState(0);
   const [correctCount, setCorrectCount] = useState(0);
   const navigate = useNavigate();
-  const listNum = foodListDummy.length;
   const [showModal, setShowModal] = useState(false);
   const [quizData, setQuizData] = useState<CardProps[]>([]);
 
   useEffect(() => {
     // const phone = localStorage.getItem("userPhone");
     //여기에 get
-    
+
     const fetchQuiz = async () => {
       setShowModal(true); // 모달 보여주기
       try {
         const response = await getQuiz(); // 실제 API 호출
-        const rawQuizzes: CardProps[] = response.result.quizzes.slice(0,3);
+        const rawQuizzes: CardProps[] = response.result.quizzes.slice(0, 3);
 
         const formatted = rawQuizzes.map((quiz) => {
-        const answerIndex = quiz.choiceDtoList.findIndex(
-          (choice) => choice.id === quiz.correctChoiceId
-        );
+          const answerIndex = quiz.choiceDtoList.findIndex(
+            (choice) => choice.id === quiz.correctChoiceId
+          );
 
-        return {
-          id: quiz.id,
-          question: quiz.question,
-          correctChoiceId: answerIndex,
-          choiceDtoList: quiz.choiceDtoList,
-        };
+          return {
+            id: quiz.id,
+            question: quiz.question,
+            correctChoiceId: answerIndex,
+            choiceDtoList: quiz.choiceDtoList,
+          };
         });
         setQuizData(formatted);
       } catch (err) {
@@ -60,30 +58,30 @@ export default function QuizPage() {
     setShowModal(true);
   }, []);
 
-	const handleOptionClick = (option: number) => {
+  const handleOptionClick = (option: number) => {
     const isCorrect = option === quizData[currentQuiz].correctChoiceId;
 
     if (isCorrect) {
       setCorrectCount((prev) => prev + 1);
     }
-		if (currentQuiz < quizData.length - 1) {
-			setCurrentQuiz((prev) => prev + 1);
-		} else {
-			if (correctCount + (option === quizData[currentQuiz].correctChoiceId ? 1 : 0) === quizData.length) {
-				navigate("/team5/quiz/quizSuccess");
-			} else {
-				navigate("/team5/quiz/quizFail");
-			}
-		}
-	};
+    if (currentQuiz < quizData.length - 1) {
+      setCurrentQuiz((prev) => prev + 1);
+    } else {
+      if (correctCount + (option === quizData[currentQuiz].correctChoiceId ? 1 : 0) === quizData.length) {
+        navigate("/team5/quiz/quizSuccess");
+      } else {
+        navigate("/team5/quiz/quizFail");
+      }
+    }
+  };
 
-	const progressPercent = ((currentQuiz) / quizData.length) * 100;
+  const progressPercent = ((currentQuiz) / quizData.length) * 100;
 
   return (
     <S.Container>
       {showModal && <PhoneModal onClose={() => setShowModal(false)} />}
-        <S.Text>오늘의 학식 골든벨 퀴즈</S.Text>
-        <HalfCircleProgress progress={progressPercent} /> 
+      <S.Text>오늘의 학식 골든벨 퀴즈</S.Text>
+      <HalfCircleProgress progress={progressPercent} />
       {quizData.length > 0 && (
         <>
           <S.QuizNumDiv>
@@ -96,21 +94,21 @@ export default function QuizPage() {
           <S.QuizBox>
             <S.Question>{quizData[currentQuiz].question}</S.Question>
             <S.Options>
-            {quizData[currentQuiz].choiceDtoList.map((opt, idx) =>
-              opt.content.trim() !== "" && (
-                <S.Option key={idx} onClick={() => handleOptionClick(idx)}>
-                  {opt.content}
-                </S.Option>
-              )
-            )}
-          </S.Options>
+              {quizData[currentQuiz].choiceDtoList.map((opt, idx) =>
+                opt.content.trim() !== "" && (
+                  <S.Option key={idx} onClick={() => handleOptionClick(idx)}>
+                    {opt.content}
+                  </S.Option>
+                )
+              )}
+            </S.Options>
 
           </S.QuizBox>
         </>
       )}
       <S.ButtonBox>
-        <S.SchoolRestaurantBtn onClick={() => navigate("/team5/foodList")}><S.BtnImg src={FileImg}/><div><p>학교 근처 맛집</p><p>{listNum}개</p></div></S.SchoolRestaurantBtn>
-        <S.VoteBtn onClick={()=>navigate('/team5/guessRate')}><S.BtnImg src={Carrot}/><p>오늘의 학식</p><p>골든벨 퀴즈</p></S.VoteBtn>
+        <S.SchoolRestaurantBtn onClick={() => navigate("/team5/foodList")}><S.BtnImg src={FileImg} /><div><p>학교 근처 맛집</p></div></S.SchoolRestaurantBtn>
+        <S.VoteBtn onClick={() => navigate('/team5/guessRate')}><S.BtnImg src={Carrot} /><p>오늘의 학식</p><p>골든벨 퀴즈</p></S.VoteBtn>
       </S.ButtonBox>
     </S.Container>
   );
